07ae4bd8d85a1678a144c67ce6eac4aa2d8d995e
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-14 20:52:42 -0300

    Feat: Criando o repository e o service da classe order

---
+++ src/main/java/com/fernandes/courseSpring/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.fernandes.courseSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fernandes.courseSpring.entities.Order;
+import com.fernandes.courseSpring.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order finById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+
+}
