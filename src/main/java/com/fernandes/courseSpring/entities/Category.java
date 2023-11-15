f092b1a779658ef355349b78e9349520479ea12d
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-15 09:50:55 -0300

    Feat: Criando as anotações do spring

--- src/main/java/com/fernandes/courseSpring/entities/Category.java
+++ src/main/java/com/fernandes/courseSpring/entities/Category.java
@@ -2,8 +2,19 @@ package com.fernandes.courseSpring.entities;
 
 import java.util.Objects;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
