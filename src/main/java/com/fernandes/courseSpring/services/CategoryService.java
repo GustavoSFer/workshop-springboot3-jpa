ea97d0ae77f3e09633e9022e0b0e3abb626910af
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-15 10:10:55 -0300

    Feat: Criando o nosso Service de Category

---
+++ src/main/java/com/fernandes/courseSpring/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.fernandes.courseSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.fernandes.courseSpring.entities.Category;
+import com.fernandes.courseSpring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	CategoryRepository category;
+	
+	public List<Category> findAll() {
+		return category.findAll();
+	}
+	
+	public Category finById(Long id) {
+		Optional<Category> obj = category.findById(id);
+		
+		return obj.get();
+	}
+}
