63a2de63ea98155d9ed8995881fc94792bb64ef6
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-15 10:06:06 -0300

    Feat: Criando o nosso repository(Model) de Category e colocando alguns valores para salvar no banco

--- src/main/java/com/fernandes/courseSpring/config/TestConfig.java
+++ src/main/java/com/fernandes/courseSpring/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.fernandes.courseSpring.entities.Category;
 import com.fernandes.courseSpring.entities.Order;
 import com.fernandes.courseSpring.entities.User;
 import com.fernandes.courseSpring.entities.enums.OrderStatus;
+import com.fernandes.courseSpring.repositories.CategoryRepository;
 import com.fernandes.courseSpring.repositories.OrderRepository;
 import com.fernandes.courseSpring.repositories.UserRepository;
 
@@ -23,9 +25,17 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	OrderRepository orderRepository;
+	
+	@Autowired
+	CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category c1 = new Category(null, "Tecnology");
+		Category c2 = new Category(null, "Eletronic");
+		Category c3 = new Category(null, "Book");
+		
 		User u1 = new User("Pedro Silva", "Silva@gmail.com", "11259554521", "159874");
 		User u2 = new User("Amanda", "amandinha@gmail.com", "1128885521", "1500074");
 		
@@ -35,6 +45,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1 ,o2, o3));
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
