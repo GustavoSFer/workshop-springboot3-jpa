abbde20eeda619ef6cc3543a069a35216004ec24
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-15 21:15:32 -0300

    Feat: Criando as anotações do spring

--- src/main/java/com/fernandes/courseSpring/entities/OrderItem.java
+++ src/main/java/com/fernandes/courseSpring/entities/OrderItem.java
@@ -5,10 +5,17 @@ import java.util.Objects;
 
 import com.fernandes.courseSpring.entities.pk.OrderItemPk;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId
 	private OrderItemPk id;
 	private int quantity;
 	private double price;
