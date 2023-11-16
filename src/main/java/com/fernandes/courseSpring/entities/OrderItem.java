66a0da53395728b99091cab94d52a92df8e09501
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-15 21:13:21 -0300

    Feat: Criando a classe OrderItem

---
+++ src/main/java/com/fernandes/courseSpring/entities/OrderItem.java
@@ -0,0 +1,76 @@
+package com.fernandes.courseSpring.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fernandes.courseSpring.entities.pk.OrderItemPk;
+
+public class OrderItem implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private OrderItemPk id;
+	private int quantity;
+	private double price;
+
+	public OrderItem() {
+	}
+
+	public OrderItem(Order order, Product product, int quantity, double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	};
+
+}
