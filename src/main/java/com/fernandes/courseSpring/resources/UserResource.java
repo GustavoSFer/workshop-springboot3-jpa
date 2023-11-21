21b5a5c1fc863c983b82751f28040a7175e9d35f
GustavoSFer <gustavo.sfernander@gmail.com>
2023-11-21 20:50:49 -0300

    Feat: Criando o insert de usuarios.

--- src/main/java/com/fernandes/courseSpring/resources/UserResource.java
+++ src/main/java/com/fernandes/courseSpring/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.fernandes.courseSpring.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fernandes.courseSpring.entities.User;
 import com.fernandes.courseSpring.services.UserService;
@@ -31,5 +35,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		User obj = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdL()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
